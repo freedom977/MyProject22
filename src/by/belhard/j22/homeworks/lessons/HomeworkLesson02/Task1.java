107c2b36f90ba79aa73b496e99c8badf32c813b9
Admin1 <leha333333@mail.ru>
2020-04-20 22:45:32 +0300

    init

---
+++ src/by/belhard/j22/homeworks/lessons/HomeworkLesson02/Task1.java
@@ -0,0 +1,14 @@
+package by.belhard.j22.homeworks.lessons.HomeworkLesson02;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int a=5;
+        if (a <= 5) {
+            System.out.println("Надень шапку");
+        }else if (a<=10) {
+            System.out.println("Сними шапку");
+        }else {
+            System.out.println("Тепло, можешь и так идти");
+        }
+    }
+}
