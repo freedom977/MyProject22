c55af7f8517bdd0e1e314aa0a753a5e2820b540b
Admin1 <leha333333@mail.ru>
2020-04-24 14:11:15 +0300

    init

---
+++ src/by/belhard/j22/homeworks/lessons/HomeWorksLesson03/Task9/Washer.java
@@ -0,0 +1,32 @@
+package by.belhard.j22.homeworks.lessons.HomeWorksLesson03.Task9;
+
+public class Washer {
+    private String powder;
+    private Type type;
+    private int temp;
+    private int time;
+
+    public Washer( Type type, int temp, int time) {
+
+        this.type = type;
+        this.temp = temp;
+        this.time = time;
+    }
+
+    public void pow (String powder) {
+        System.out.println("Беру порошок " + powder);
+    }
+    public void wash (){
+        System.out.println("Стираю");
+    }
+    public int temp (){
+        return temp;
+    }
+    public int time () {
+        return time;
+    }
+
+    public Type getType() {
+        return type;
+    }
+}
