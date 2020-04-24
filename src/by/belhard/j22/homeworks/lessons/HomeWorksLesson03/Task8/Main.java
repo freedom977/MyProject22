6403572aa683ab76c7c79174935ef761def16eb8
Admin1 <leha333333@mail.ru>
2020-04-24 13:40:57 +0300

    init

---
+++ src/by/belhard/j22/homeworks/lessons/HomeWorksLesson03/Task8/Main.java
@@ -0,0 +1,11 @@
+package by.belhard.j22.homeworks.lessons.HomeWorksLesson03.Task8;
+
+public class Main {
+    public static void main(String[] args) {
+        Gamer gamer = new Gamer(8, 12 );
+
+        gamer.run();
+
+       System.out.println("x: " + gamer.run1() + " " + "y: " + gamer.run2());
+    }
+}
