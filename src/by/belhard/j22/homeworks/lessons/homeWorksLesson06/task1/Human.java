17575b09728633007165da7644fbe050be3f8e52
Admin1 <leha333333@mail.ru>
2020-05-10 08:21:54 +0300

    init

--- src/by/belhard/j22/homeworks/lessons/homeWorksLesson06/task1/Human.java
+++ src/by/belhard/j22/homeworks/lessons/homeWorksLesson06/task1/Human.java
@@ -1,8 +1,20 @@
 package by.belhard.j22.homeworks.lessons.homeWorksLesson06.task1;
 
+import by.belhard.j22.lessons.lesson06.test.Cal;
+
 public class Human {
 
     String pitomec;
+
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Fish fish = new Fish();
+
+        cat.game();
+        dog.game();
+        fish.game();
+    }
 
 
 }
